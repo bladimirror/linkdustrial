78e984183c8781fb95d8cde5aedc6d76451936d6
bladimirror <christian.b.martinez@gmail.com>
2017-05-11 07:15:04 -0700

    updated CSS files (RWD), application.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,12 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( min-1336px_83em.scss )
+Rails.application.config.assets.precompile += %w( min-640px_40em.scss )
+Rails.application.config.assets.precompile += %w( max-640px_40em.scss )
+
+ 
+#<%= stylesheet_link_tag "min-640px_40em", media: "(min-width: 40em)" %>
+#<%= stylesheet_link_tag "portrait", media: "(orientation: portrait)" %>
+#<%= stylesheet_link_tag "landscape", media: "(orientation: landscape)" %>
+
